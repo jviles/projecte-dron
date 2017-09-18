e4c236acc48d84e36705456660569e571c4b5441
jviles <jvilesheras@hotmail.com>
2017-09-18 18:40:10 +0200

    map implemented

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express        = require("express");
 const path           = require("path");
+var favicon = require('serve-favicon');
 const logger         = require("morgan");
+
 const cookieParser   = require("cookie-parser");
 const bodyParser     = require("body-parser");
 const mongoose       = require("mongoose");
---
+++ public/javascript/main.js
@@ -0,0 +1,18 @@
+function startMap() {
+    var ironhackBCN = {
+        lat: 41.3977381, 
+        lng: 2.190471916};
+    var map = new google.maps.Map(
+      document.getElementById('map'), 
+      {
+        zoom: 15,
+        center: ironhackBCN
+      }
+    );
+  }
+  
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    startMap();
+});
+
