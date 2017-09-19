3fc477eee25544870de272da3537e01e09f96ca8
jviles <jvilesheras@hotmail.com>
2017-09-19 10:35:35 +0200

    Map location included

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -8,7 +8,69 @@ function startMap() {
         zoom: 15,
         center: ironhackBCN
       }
-    );
+      
+    );     
+    
+    var geocoder = new google.maps.Geocoder();    
+    var infoWindow = new google.maps.InfoWindow;
+
+
+    if (navigator.geolocation) {
+      
+          navigator.geolocation.getCurrentPosition(function (position) {
+      
+            var pos = {
+      
+              lat: position.coords.latitude,
+      
+              lng: position.coords.longitude
+      
+            };
+      
+      
+      
+            infoWindow.setPosition(pos);
+      
+            infoWindow.setContent('Location found.');
+      
+            infoWindow.open(map);
+      
+            map.setCenter(pos);
+      
+      
+      
+          }, function () {
+      
+            handleLocationError(true, infoWindow, map.getCenter());
+      
+          });
+      
+        } else {
+      
+          // Browser doesn't support Geolocation
+      
+          handleLocationError(false, infoWindow, map.getCenter());
+      
+        }
+      
+      
+      
+      
+      
+        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      
+          infoWindow.setPosition(pos);
+      
+          infoWindow.setContent(browserHasGeolocation ?
+      
+            'Error: The Geolocation service failed.' :
+      
+            'Error: Your browser doesn\'t support geolocation.');
+      
+          infoWindow.open(map);
+      
+        }
+
   }
   
 
