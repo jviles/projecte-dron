df2a113a13067365aedfaf142cab7642debf01a2
jviles <jvilesheras@hotmail.com>
2017-09-20 15:31:53 +0200

    trying to do google maps

--- routes/race.js
+++ routes/race.js
@@ -1,5 +1,6 @@
 const express 	   = require("express");
 const router       = express.Router();
+const Race         = require('../model/race.js');
 
 
 
@@ -17,47 +18,15 @@ router.use((req, res, next) => {
 
 // --- race profile
 
-router.get('/',(req, res, next) => {
-    console.log("GET profile")
-    res.render('race');
-  });
-
-/*router.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
     Race.find({}, (err, docs) => {
       if(err) {
         next();
       }
       else {
-        res.render('/race', {Race:docs});
+        res.render('race', {Race:docs});
       }
     });
-  });*/
-  
-  
-  /*
-  router.get('/new', (req, res, next) => {
-    res.render('drones/new');
   });
-  
-  router.post('/', (req, res, next) => {
-    var newDroneName = req.body.droneName;
-    var newPropellers = req.body.propellers;
-    var newMaxSpeed = req.body.maxSpeed;
-  
-    _addToDBS(newDroneName, newPropellers, newMaxSpeed);
-    res.redirect('/drones');
-  });
-  
-  function _addToDBS(name, prop, speed){
-    var newDrone = new Drone({droneName: name, propellers: prop, maxSpeed: speed});
-    newDrone.save((err, doc) => {
-      if(err){
-        throw err;
-      }
-      else{
-        console.log(`${doc} is saved!`);
-      }
-    });
-  }*/
-  
+
   module.exports = router;
